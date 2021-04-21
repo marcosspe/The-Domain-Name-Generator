b20191fc23ab159c17190a09c77c5f1c976161e1
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-21 15:54:23 +0000

    marcos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var domain = [".com", ".uy"];
+
+  function domainGenerator() {
+    var resp;
+    for (var i = 0; i < pronoun.length; i++) {
+      for (var j = 0; j < adj.length; j++) {
+        for (var k = 0; k < noun.length; k++) {
+          for (var l = 0; l < domain.length; l++) {
+            resp = pronoun[i] + adj[j] + noun[k] + domain[l];
+          }
+        }
+      }
+    }
+    return resp;
+  }
+  let domainText = document.getElementById("listaDominios");
+  let btnGenerator = document.getElementById("btnGenerator");
+
+  btnGenerator.addEventListener("click", function() {
+    domainText.innerHTML = domainGenerator();
+  });
   console.log("Hello Rigo from the console!");
 };

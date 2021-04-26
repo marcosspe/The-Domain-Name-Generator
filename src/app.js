c008317d024d36f7c5c00d44f4b0395a24596386
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-26 17:19:44 +0000

    marcos

--- src/app.js
+++ src/app.js
@@ -1,35 +1,31 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  var pronoun = ["the", "our"];
-  var adj = ["great", "big"];
-  var noun = ["jogger", "racoon"];
-  var domain = [".com", ".uy"];
-
   function domainGenerator() {
-    var resp;
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domain = [".com", ".uy"];
+    let domainGenerated = [];
     for (var i = 0; i < pronoun.length; i++) {
       for (var j = 0; j < adj.length; j++) {
         for (var k = 0; k < noun.length; k++) {
           for (var l = 0; l < domain.length; l++) {
-            resp += pronoun[i] + adj[j] + noun[k] + domain[l];
+            domainGenerated.push(pronoun[i] + adj[j] + noun[k] + domain[l]);
           }
         }
       }
     }
-    return resp;
+    let domainIndex = Math.floor(Math.random() * domainGenerated.length);
+    let randomDomain = domainGenerated[domainIndex];
+    return randomDomain;
   }
   let domainText = document.getElementById("domain");
   let btnGenerator = document.getElementById("btnGenerator");
-
   btnGenerator.addEventListener("click", function() {
     domainText.innerHTML = domainGenerator();
   });
-  console.log("Hello Rigo from the console!");
 };
